6e6dec81e5c006019712ba7e1e36c8c7bd05d9eb
Eden Mesfun <emesfun@gmail.com>
2017-12-17 23:21:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// write your code below!
+//NUMBER ONE (test passed)
+
+
+function happyHolidays(string)
+{
+ if (string === string)
+  {return ('Happy holidays!')}
+}
+  
+
+//NUMBER TWO  
+
+// var holiday = "Independence Day";
+// var days = "20";
+// var name = "you";
+// var holiday = "holidays"
+
+function happyHolidaysTo(name)
+{
+  if ("Happy holidays, you!" === "Happy holidays, you!")
+   {return ("Happy holidays, ${name}!")}
+}
+
+
+//NUMBER THREE
+
+function happyHolidayTo(holiday, name)
+{ if ('Independence Day', 'you' === 'Happy Independence Day, you!')
+return ('Happy ${holiday}, ${name}!')
+}
+ 
+//NUMBER FOUR
+
+function holidayCountdown(holiday, days){
+  if ("It\'s ${days} days until ${holiday}!" === "It\'s 20 days until Mother\'s Day!")
+  {return ("It\'s ${days} days until ${holiday}!")}
+  
+}
+ 
